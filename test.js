204b9cef021a146340c20281225039ca1897e4a8
Finney Osajere <osafinney21@gmail.com>
2024-09-26 09:51:41 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const { Client } = require('whatsapp-web.js');
 const client = new Client();
 
 client.on('qr', (qr) => {
-    console.log('QR RECEIVED', qr);
+    qrcode.generate(qr, { small: true });
 });
 
 client.on('ready', () => {
